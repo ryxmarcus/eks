a2537325bd503bb05438bd12c93d53bafb82fcb2
ryxmarcus <128896074+ryxmarcus@users.noreply.github.com>
2024-11-04 09:13:02 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,22 +1,28 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 4.0"
-
+  
   name = local.name
   cidr = local.vpc_cidr
-
+  
   azs             = local.azs
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
   intra_subnets   = local.intra_subnets
-
+  
   enable_nat_gateway = true
-
+  
+  # Enable auto-assign public IP for public subnets
+  map_public_ip_on_launch = true
+  
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
+    # Add this tag for EKS
+    "kubernetes.io/cluster/${local.name}" = "shared"
   }
-
+  
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/cluster/${local.name}" = "shared"
   }
 }
